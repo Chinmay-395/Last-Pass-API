f5af59fc523a69590b63ddef21f694712ff3211a
Chinmay-395 <chinmaydali395@gmail.com>
2020-07-20 21:27:46 +0530

    Successfully added redux-persist for both auth and lp_data

--- FrontEnd/lastpass_reactjs/src/App.js
+++ FrontEnd/lastpass_reactjs/src/App.js
@@ -2,15 +2,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
+import { PersistGate } from 'redux-persist/integration/react'
 //Ant-Design imports
 import 'antd/dist/antd.css';
 //Custom imports
 import './App.css';
 import Main from './components/MainComponent';
 import { configureStore } from './redux/configureStore'
+// import {}
 
-const store = configureStore();
+
+
+console.log("********The ConfiguredStore********", configureStore())
+console.log("********The Store********", configureStore().store)
+console.log("********The persistor********", configureStore().persistor)
+const store = configureStore().store;
+const persistor = configureStore().persistor
 
 
 class App extends Component {
@@ -19,9 +26,11 @@ class App extends Component {
     return (
       <Provider store={store}>
         <BrowserRouter>
-          <div className="App">
-            <Main />
-          </div>
+          <PersistGate persistor={persistor}>
+            <div className="App">
+              <Main />
+            </div>
+          </PersistGate>
         </BrowserRouter>
       </Provider>
     );
--- FrontEnd/lastpass_reactjs/src/redux/configureStore.js
+++ FrontEnd/lastpass_reactjs/src/redux/configureStore.js
@@ -2,16 +2,20 @@
 import thunk from 'redux-thunk';
 import logger from 'redux-logger'
 import { createStore, combineReducers, applyMiddleware } from 'redux';
+////React-Redux-Persist
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 //custom imports
-import { Auth_reducer } from './auth_reducer'
-import { Lp_Data_reducer } from './lp_data_reducer'
+////importing all the reducers
+/* import { Auth_reducer } from './auth_reducer'
+ import { Lp_Data_reducer } from './lp_data_reducer'*/
+import rootReducer from './root_reducer'
+
 
 export const configureStore = () => {
     const store = createStore(
-        combineReducers({
-            auth: Auth_reducer,
-            lp_data: Lp_Data_reducer
-        }), applyMiddleware(thunk, logger)
+        rootReducer, applyMiddleware(thunk, logger)
     );
-    return store;
+    const persistor = persistStore(store);
+    return { store, persistor };
 }
