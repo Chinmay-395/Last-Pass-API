a237ad2c20be08385411d5615a91203354186ab7
Chinmay-395 <chinmaydali395@gmail.com>
2020-09-06 12:11:39 +0530

    Customized django admin panel a little bit

--- cns_project/lastpass_api/admin.py
+++ cns_project/lastpass_api/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import LastPassUserData
 # Register your models here.
-admin.site.register(LastPassUserData)
+
+
+class LastPassUserDataAdmin(admin.ModelAdmin):
+    list_display = ['ogUser', 'name_of_website', 'url_of_website']
+    search_fields = ['name_of_website']
+    list_display_links = ['name_of_website']
+    list_filter = ["ogUser"]
+
+    class Meta:
+        model = LastPassUserData
+
+
+admin.site.register(LastPassUserData, LastPassUserDataAdmin)
